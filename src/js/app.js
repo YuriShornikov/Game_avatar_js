c20b04d8b860655b5218c25c405fd4e6c3ee38d5
Yuri Shornikov <dbonlyfun@gmail.com>
2024-03-26 23:03:54 +0300

    add class

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,39 @@
 import goblin from "../img/goblin.png";
 
-// Создание аватарки
-const character = document.createElement("img");
-character.src = goblin;
-character.classList.add("character");
+class Game {
+  constructor() {
+    this.boardSize = 4;
+    this.character = 0;
+    this.board = 0;
+    this.init();
+  }
 
-// Добавление в поле
-const gameBoard = document.querySelector(".game-board");
-gameBoard.appendChild(character);
+  init() {
+    this.createBoard();
+    this.createCharacter();
+    setInterval(() => this.moveCharacter(), 1000);
+  }
 
-// Функция рандомной позиции
-function getRandomPosition() {
-  const row = Math.floor(Math.random() * 4);
-  const column = Math.floor(Math.random() * 4);
-  return [row, column];
+  createBoard() {
+    this.board = document.querySelector('.game-board');
+  }
+
+  // Создание гоблина
+  createCharacter() {
+    this.character = document.createElement('img');
+    this.character.src = goblin;
+    this.character.className = 'character';
+    this.board.appendChild(this.character);
+    
+  }
+
+  // Перемещение внутри 
+  moveCharacter() {
+    const row = Math.floor(Math.random() * this.boardSize) + 1;
+    const column = Math.floor(Math.random() * this.boardSize) + 1;
+    this.character.style.gridColumn = column;
+    this.character.style.gridRow = row;
+  }
 }
 
-// функция перемещения
-function moveCharacter() {
-  const currentPosition = getRandomPosition();
-  character.style.gridColumn = currentPosition[1] + 1;
-  character.style.gridRow = currentPosition[0] + 1;
-}
-
-// Интервал
-setInterval(moveCharacter, 2000);
+const game = new Game();
